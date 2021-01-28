0fb596d50d2bd605082b53cdcb6c3fadb18fc013
悦航 张 <arsizeof3416@gmail.com>
2021-01-28 16:25:28 +0800

    ble

--- utils/btls/ble.js
+++ utils/btls/ble.js
@@ -1,16 +1,5 @@
 import BLEHandler from "./bleHandler"
 
-/**
- * 蓝牙连接流程：
- * 1.先初始化蓝牙适配器
- * 2.搜寻附近的蓝牙外围设备
- * 3.获取设备ID
- * 4.停止搜寻
- * 5.开始连接
- * 6.获取service
- * 7.获取特征值
- */
-
 class BLE extends BLEHandler {
     constructor(blename) {
         super(blename)
