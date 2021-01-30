7b12c8cb5d6509d154426c092bd4ec514501caa3
悦航 张 <arsizeof3416@gmail.com>
2021-01-30 18:52:19 +0800

    fix:serviceid

--- utils/btls/bleHandler.js
+++ utils/btls/bleHandler.js
@@ -13,7 +13,7 @@ class BLEHandler {
         this.writeCharacteristicId = "";
         this.notifyCharacteristicId = "";
         this.deviceId = "";
-        this.serviceId = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
+        this.serviceId = "6xxx";
     }
     async openAdapter() {
         let [err, res] = await t._openAdapter.call(this);
